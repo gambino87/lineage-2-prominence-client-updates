c521b758b333c82f8e6a906e7d01699cf93cbf1c
gambino87 <vargas87@gmail.com>
2026-09-11 17:12:01 -0700

    fix: keep keybind button visible after refresh

--- launcher/LauncherUpdate.cs
+++ launcher/LauncherUpdate.cs
@@ -35,7 +35,7 @@ public static class BundledTools {
  }
 }
 public static class LauncherUpdate {
- public const string Version="1.1.3";
+ public const string Version="1.1.4";
 #if TEST_BENCH
  public const string Channel="test-bench";
 #else
